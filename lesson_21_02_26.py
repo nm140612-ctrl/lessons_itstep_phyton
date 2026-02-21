c76dded9fd8999c32ec8e6f69dd2b61df2dbb16d
nm140612-ctrl <nm140612@gmail.com>
2026-02-21 17:07:49 +0200

    3 commit

---
+++ lesson_21_02_26.py
@@ -0,0 +1,28 @@
+"""
+def назва_функції(аргументи):
+    код функції
+
+назва_функції(значення)
+"""
+
+# # однорядковий коментар
+# print('''
+# однорядковий комента
+# однорядковий коментар
+# однорядковий коментар
+# ''')
+
+def first_function():
+    print('Hello world')
+
+first_function()
+print(first_function)
+def second_function():
+    hello = "Hello goods"
+    return hello
+print(second_function)
+print(second_function())
+
+
+
+
